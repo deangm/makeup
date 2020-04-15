5e4f586630c4a569d038dc66dc0759477b82e064
mikey <michael.d.deangelo@Gmail.com>
2020-04-15 09:05:47 -0600

    file name change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
-import { ProductComponent } from './components/product/product.component';
+import { ProductComponent } from './components/product-detail/product.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import {HttpClientModule} from '@angular/common/http';
 
