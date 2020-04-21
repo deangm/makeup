ee547e3e3bcc81463273be1d68056eebf3c7e0c0
mikey <michael.d.deangelo@Gmail.com>
2020-04-21 08:22:47 -0600

    binding select in details

--- src/app/components/product-detail/product.component.ts
+++ src/app/components/product-detail/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -11,13 +12,17 @@ import { ProductsService } from 'src/app/services/products.service';
 export class ProductComponent implements OnInit {
 
   product
+  form: FormGroup;
   constructor(
     private productsService: ProductsService,
-    private route: ActivatedRoute
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.product = this.productsService.selectedProduct
+    this.form = this.fb.group({
+      color: "Yellow"
+    })
   }
 
 }
