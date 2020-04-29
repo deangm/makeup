fde18bbb76c6af6c652cf6c58e520105fbe54aa5
grizzsr <justincllj@gmail.com>
2020-04-29 08:58:47 -0600

    added the auth guard

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthProvider } from 'ngx-auth-firebaseui';
+import { User, UserInfo } from 'firebase/app';
+import { Observable } from 'rxjs';
 
 
 
@@ -13,12 +15,16 @@ export class LoginComponent implements OnInit {
 
   providers = AuthProvider;
 
+  user: User;
+  user$: Observable<User>;
+
   constructor(
     // public auth: AuthService,
     private router: Router
     ) {}
 
   ngOnInit(): void {
+
   }
 
   printUser(event) {
