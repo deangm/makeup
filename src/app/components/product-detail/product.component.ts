eb6a951d74d46150f3fe0bb84104c96982c8ee92
mikey <michael.d.deangelo@Gmail.com>
2020-04-30 08:45:17 -0600

    changes to reviews

--- src/app/components/product-detail/product.component.ts
+++ src/app/components/product-detail/product.component.ts
@@ -29,7 +29,7 @@ import { ReviewsService } from 'src/app/services/reviews.service';
 export class ProductComponent implements OnInit {
 
 
-  product = data['default'][0]
+  product 
   color = "default"
   userid
 
@@ -56,7 +56,7 @@ export class ProductComponent implements OnInit {
       this.reviews = reviews.filter(review => review.product_id == this.route.snapshot.params.id)
     })
     
-    // this.product = this.productsService.selectedProduct
+    this.product = this.productsService.selectedProduct
   }
 
   animateMessage() {
